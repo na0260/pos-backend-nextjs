359cb5862fe15c013b336feb9b02936721b9ac5d
na0260 <samjr.008@gmail.com>
2024-09-28 02:46:12 +0600

    middleware function created

---
+++ src/middleware.js
@@ -0,0 +1,27 @@
+import {NextResponse} from "next/server";
+import {verifyToken} from "./utility/JWTHelper";
+
+export async function middleware(req){
+    if (req.nextUrl.pathname.startsWith('/api/v1/dashboard')){
+        try {
+            let token = req.cookies.get('token');
+            let payload = await verifyToken(token['value']);
+
+            const reqHeader = new Headers(req.headers);
+            reqHeader.set('email', payload['email']);
+            reqHeader.set('id', payload['id']);
+
+            return NextResponse.next({
+                request:{
+                    headers: reqHeader
+                }
+            });
+        }catch (e) {
+            return NextResponse.json({status: 'failed', data: 'Unauthorized'}, {status: 401})
+        }
+    }
+
+    if (req.nextUrl.pathname.startsWith('/api/v1/user')){
+        return  NextResponse.next();
+    }
+}
