3af1fd99c26824400851ba6c1917fa65e7d055c7
na0260 <samjr.008@gmail.com>
2024-09-28 22:47:46 +0600

    get Products by category id added

--- src/app/api/v1/dashboard/product/route.js
+++ src/app/api/v1/dashboard/product/route.js
@@ -2,18 +2,41 @@ import {NextResponse} from "next/server";
 import {headers} from "next/headers";
 import {PrismaClient} from "@prisma/client";
 
-export async function GET(){
+export async function GET(req){
     try {
         const headerList = headers();
         const id = headerList.get("id");
         const prisma = new PrismaClient();
-        const products = await prisma.products.findMany({
-            where:{
-                user_id: parseInt(id)
-            }
-        });
+        const {searchParams} = new URL(req.url);
+        const categoryId = searchParams.get("cat_id");
+        if (categoryId){
+            const category = await prisma.categories.findUnique({
+                where:{
+                    id: parseInt(categoryId),
+                    user_id: parseInt(id)
+                }
+            });
+            if (!category){
+                return NextResponse.json({status: "Failed", data: "Category not found"}, {status: 404});
+            }else {
+                const products = await prisma.products.findMany({
+                    where:{
+                        user_id: parseInt(id),
+                        category_id: parseInt(categoryId)
+                    }
+                });
 
-        return NextResponse.json({status: "Success", data: products}, {status: 200});
+                return NextResponse.json({status: "Success", data: products}, {status: 200});
+            }
+        }else {
+            const products = await prisma.products.findMany({
+                where:{
+                    user_id: parseInt(id)
+                }
+            });
+
+            return NextResponse.json({status: "Success", data: products}, {status: 200});
+        }
     }catch (e) {
         return NextResponse.json({status: "Failed", data: e.message}, {status: 500});
     }
